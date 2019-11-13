34bf83087cbb08acb5005b4922f6ac2204955d8b
mrale <mr.alex.pan@list.ru>
2019-11-13 19:57:08 +0300

    Я написал привет Мир 3 раза

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ def main():
     print('Hello World!!!')
     print('Hello World!!!')
     print('Hello World!!!')
+    print('Hello World!!!')
+    print('Hello World!!!')
+    print('Hello World!!!')
 
 if __name__ == '__main__':
     main()
