bda82fba5738ee5813e418d572fa0e4c415ddae8
mrale <mr.alex.pan@list.ru>
2019-11-15 13:20:27 +0300

    Присоединил класс формы, вместо файла формы

--- random_circles.py
+++ random_circles.py
@@ -5,12 +5,14 @@ from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
 # from PyQt5.QtCore import QPainter
 from PyQt5.QtGui import QPainter, QColor
 import random
+from ui_randomcircle import Ui_Form
 
 
-class MyWidget(QMainWindow):
+class MyWidget(QMainWindow, Ui_Form):
     def __init__(self):
+        # super().__init__()
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.run)
         self.cnt = 0
         self.Flag = False
